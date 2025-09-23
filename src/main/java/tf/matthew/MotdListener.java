f26861f2585a147b2a4384454033ef415db5004e
matthew.tf <matthewpts@outlook.com>
2025-09-23 19:35:15 -0400

    feat: upgrade project to Java 17 and refactor MOTD config
    Improve code and add a lot of new features.
    
    - Updated Java version from 1.8 to 17 in pom.xml and IDEA settings
    - Refactored config.yml to use list format with comments and newlines for better MOTD readability
    - Updated README.md (binary diff indicates content changes)
    
    With love,
    Matt.

--- src/main/java/tf/matthew/MotdListener.java
+++ src/main/java/tf/matthew/MotdListener.java
@@ -1,4 +1,4 @@
-package com.lolmotd;
+package tf.matthew;
 
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -9,7 +9,7 @@ public class MotdListener implements Listener {
     @EventHandler
     public void onProxyPing(ProxyPingEvent event) {
         int protocolVersion = event.getConnection().getVersion();
-        String motd = protocolVersion < 735 ? LOLMotd.legacyMotd : LOLMotd.modernMotd;
+        String motd = protocolVersion < 735 ? String.join("\n", LOLMotd.getInstance().getLegacyMotd()) : String.join("\n", LOLMotd.getInstance().getModernMotd());
         event.getResponse().setDescription(motd);
     }
 }
--- src/main/java/tf/matthew/ReloadCommand.java
+++ src/main/java/tf/matthew/ReloadCommand.java
@@ -1,14 +1,14 @@
-package com.lolmotd;
+package tf.matthew;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
+import net.md_5.bungee.api.chat.TextComponent;
 
 import java.util.Collections;
 
 public class ReloadCommand extends Command implements TabExecutor {
-
     public ReloadCommand() {
         super("lolmotd", "lolmotd.reload");
     }
@@ -17,9 +17,13 @@ public class ReloadCommand extends Command implements TabExecutor {
     public void execute(CommandSender sender, String[] args) {
         if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
             LOLMotd.getInstance().loadConfig();
-            sender.sendMessage(ChatColor.GREEN + "[LOLMotd] Configuration reloaded.");
+            sender.sendMessage(new TextComponent(ChatColor.GREEN + "Configuration reloaded."));
         } else {
-            sender.sendMessage(ChatColor.RED + "[LOLMotd] Usage: /lolmotd reload");
+            sender.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', "&7")));
+            sender.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', "&f⭐ &a&lLOLMotd &8| &7Usage")));
+            sender.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', "&7")));
+            sender.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', "&8• &fUsage: &a/lolmotd reload")));
+            sender.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', "&7")));
         }
     }
 
